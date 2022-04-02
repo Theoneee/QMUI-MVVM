23e9d1459ed9903d3f193c8fc6dab3e0d43e85dc
theoneee <625805189@qq.com>
2022-04-02 14:51:25 +0800

    LoadService增加设置VISIBLE/GONE方式

--- core/src/main/java/com/theone/mvvm/core/base/loader/LoaderService2.kt
+++ core/src/main/java/com/theone/mvvm/core/base/loader/LoaderService2.kt
@@ -3,7 +3,10 @@ package com.theone.mvvm.core.base.loader
 import android.content.Context
 import android.os.Looper
 import android.view.View
+import android.view.View.NO_ID
 import android.view.ViewGroup
+import com.theone.common.ext.gone
+import com.theone.common.ext.visible
 import com.theone.mvvm.core.base.loader.callback.Callback
 import com.theone.mvvm.core.base.loader.callback.SuccessCallback
 
@@ -28,12 +31,12 @@ import com.theone.mvvm.core.base.loader.callback.SuccessCallback
 /**
  * @author The one
  * @date 2022-03-25 09:51
- * @describe LoaderService2
+ * @describe LoaderService
  * @email 625805189@qq.com
- * TODO 将内容层remove/add的方式
+ * TODO 将内容层VISIBLE/GONE的方式
  */
 
-class LoaderService {
+class LoaderService2 {
 
     private val TAG = this.javaClass.simpleName
 
@@ -53,7 +56,7 @@ class LoaderService {
 
     fun showSuccessPage() = showCallbackView(SuccessCallback::class.java)
 
-    fun register(target: View, builder: Loader.Builder?): LoaderService {
+    fun register(target: View, builder: Loader.Builder?): LoaderService2 {
         if (null != rootView) {
             throw RuntimeException("Loader has been registered.")
         }
@@ -64,7 +67,7 @@ class LoaderService {
                     view = target
                     callbacks.add(this)
                 }
-                preView = target
+                // preView = target
                 parent
             }
             else -> {
@@ -120,8 +123,15 @@ class LoaderService {
             if (item.javaClass == status) {
                 preCallback = status
                 if (status == SuccessCallback::class.java) {
-                    successCallback?.view?.let {
-                         replaceContentWithView(it)
+                    var preId = NO_ID
+                    preView?.let {
+                        preId = it.id
+                        ensureRootView().removeViewInLayout(it)
+                    }
+                    successCallback?.view?.run {
+                        id = preId
+                        visible()
+                        // replaceContentWithView(it)
                     }
                 } else {
                     with(item) {
@@ -141,14 +151,26 @@ class LoaderService {
 
     private fun replaceContentWithView(view: View) {
         with(ensureRootView()) {
+            var preId = NO_ID
+            successCallback?.view?.run {
+                preId = id
+                id = NO_ID
+                gone()
+            }
+            var index = 0
+            view.id =  preId
             preView?.let {
-                // 设置id,解决ConstraintLayout布局问题
                 view.id =  it.id
-                val index = indexOfChild(it)
+                // 设置id,解决ConstraintLayout布局问题
+                index = indexOfChild(it)
                 removeViewInLayout(it)
-                addView(view, index, successCallback?.view?.layoutParams)
                 // 某些机型addView后不执行此方法，这里手动执行下
-                requestApplyInsets()
+                // requestApplyInsets()
+            }
+            if(index > 0){
+                addView(view, index, loaderParams)
+            }else{
+                addView(view, loaderParams)
             }
             preView = view
         }
