92e9b41046ab8f1d4d5ce9bb90ad219f4380d9fc
theoneee <625805189@qq.com>
2022-07-04 16:58:39 +0800

    登录后请求一次数据才算签到?

--- app/src/main/java/com/theone/demo/domain/work/LoginSignWorker.kt
+++ app/src/main/java/com/theone/demo/domain/work/LoginSignWorker.kt
@@ -5,9 +5,11 @@ import android.content.Context
 import androidx.work.CoroutineWorker
 import androidx.work.ListenableWorker
 import androidx.work.WorkerParameters
+import com.theone.demo.data.net.Url
 import com.theone.demo.data.repository.ApiRepository
 import okhttp3.internal.wait
 import rxhttp.awaitResult
+import rxhttp.wrapper.cahce.CacheMode
 
 //  ┏┓　　　┏┓
 //┏┛┻━━━┛┻┓
@@ -54,6 +56,7 @@ class LoginSignWorker(context: Context, workerParams: WorkerParameters) :
                 Result.Failure()
             } else {
                 ApiRepository.INSTANCE.loginOrRegister(account, password, false).await()
+                ApiRepository.INSTANCE.getArticles(Url.HOME_ARTICLE,1, CacheMode.NETWORK_SUCCESS_WRITE_CACHE)
                 Result.success()
             }
         } catch (e: Exception) {
