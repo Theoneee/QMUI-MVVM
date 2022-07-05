08c2b6a2f210acf4d612ed41b35d10fba3ae4d49
theoneee <625805189@qq.com>
2022-07-05 09:54:43 +0800

    优化App更新界面布局

--- app/src/main/java/com/theone/demo/data/model/bean/AppUpdate.kt
+++ app/src/main/java/com/theone/demo/data/model/bean/AppUpdate.kt
@@ -30,20 +30,31 @@ import kotlinx.parcelize.Parcelize
  * @remark
  */
 @Parcelize
-class AppUpdate :IApkUpdate,Parcelable {
+class AppUpdate : IApkUpdate, Parcelable {
 
     override fun isNewVersion(): Boolean = true
 
     override fun getAppVersionCode(): Int = 1
 
-    override fun isForceUpdate(): Boolean  = false
+    override fun isForceUpdate(): Boolean = false
 
     override fun getAppVersionName(): String = "1.0.0"
 
-    override fun getAppApkUrl(): String = "http://theone.0851zy.com/2022/04/13/97096d9c4094306a804f847f7c89d751.apk"
+    override fun getAppApkUrl(): String =
+        "http://theone.0851zy.com/2022/04/13/97096d9c4094306a804f847f7c89d751.apk"
 
-    override fun getAppUpdateLog(): String = "【新增】短视频首页增加今日热门\n【新增】短视频首页增加今日热门\n【新增】短视频首页增加今日热门\n【新增】短视频首页增加今日热门\n【新增】短视频首页增加今日热门\n【新增】短视频首页增加今日热门"
+    override fun getAppUpdateLog(): String =
+        "【新增1】短视频首页增加今日热门\n" +
+                "【新增2】短视频首页增加今日热门\n" +
+                "【新增3】短视频首页增加今日热门\n" +
+                "【新增4】短视频首页增加今日热门\n" +
+                "【新增5】短视频首页增加今日热门\n" +
+                "【新增6】短视频首页增加今日热门\n" +
+                "【新增7】短视频首页增加今日热门\n" +
+                "【新增8】短视频首页增加今日热门\n" +
+                "【新增9】短视频首页增加今日热门\n" +
+                "【新增10】短视频首页增加今日热门\n"
 
-    override fun getAppApkSize(): Long  = 0
+    override fun getAppApkSize(): Long = 0
 
 }
